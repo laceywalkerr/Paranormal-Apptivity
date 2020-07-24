f426eb49c6721c6c8be2d1ca34f6ec3088b3916e
Lacey Walker <65372269+laceywalkerr@users.noreply.github.com>
2020-07-24 12:01:32 -0500

    first commit

---
+++ components/Para.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Para.css";
+
+const Para = () => {
+  return (
+    <div>
+      <h1>
+        Welcome to the home page!!!
+      </h1>
+    </div>
+  );
+};
+
+export default Para;
