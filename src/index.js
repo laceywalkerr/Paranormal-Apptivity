37773976bc277f1f60b1684405d41b2eaa1c1bce
Lacey Walker <65372269+laceywalkerr@users.noreply.github.com>
2020-07-24 12:25:52 -0500

    added favicon and linked para.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Para from './components/Para';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Para />
   </React.StrictMode>,
   document.getElementById('root')
 );
