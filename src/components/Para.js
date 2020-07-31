6121d7cd97b0be1cb00f91168f4cdb641723cce8
Lacey Walker <65372269+laceywalkerr@users.noreply.github.com>
2020-07-31 15:24:25 -0500

    passing props with login

--- src/components/Para.js
+++ src/components/Para.js
@@ -1,4 +1,5 @@
-import React from "react";
+
+import React, { useState } from 'react';
 import NavBar from "./navbar/NavBar";
 import ApplicationViews from "./ApplicationViews"
 import "./Para.css";
