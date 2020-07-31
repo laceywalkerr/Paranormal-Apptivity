751ce3cc8d4cb89f1bf94c03e1d21d47c03344e1
Lacey Walker <65372269+laceywalkerr@users.noreply.github.com>
2020-07-31 15:33:32 -0500

    logout works, login is broken

--- src/components/Para.js
+++ src/components/Para.js
@@ -13,9 +13,15 @@ const Para = () => {
     sessionStorage.setItem("credentials", JSON.stringify(user));
     setHasUser(isAuthenticated());
   };
+
+  const clearUser = () => {
+    sessionStorage.clear();
+    setHasUser(isAuthenticated());
+  }
+  
   return (
     <>
-      <NavBar hasUser={hasUser} />
+      <NavBar hasUser={hasUser} clearUser={clearUser} />
       <ApplicationViews hasUser={hasUser} setUser={setUser} />
       
     </>
